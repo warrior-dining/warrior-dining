eac701416107469a982a162445e0ab77b4a17dc6
o-tao <dhtkdals369@gmail.com>
2024-08-23 15:49:33 +0900

    spring-boot 프로젝트 생성

---
+++ src/main/java/warriordining/WarriorDiningApplication.java
@@ -0,0 +1,13 @@
+package warriordining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarriorDiningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarriorDiningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/warriordining/WarriorDiningApplicationTests.java
@@ -0,0 +1,13 @@
+package warriordining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarriorDiningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
